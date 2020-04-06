6fc3387596039aca8acc70d8f941b4b8bc5e333d
Walker-TW <thomas.w.walker@live.co.uk>
2020-04-06 21:06:55 +0100

    Changed indentation of binarySearch to reflect rubocop norms

--- binarySearch.rb
+++ binarySearch.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 def my_binary_search(list, item)
   low = 0
-  high = (list.length) - 1
+  high = list.length - 1
 
   while low <= high
     mid = (low + high)
@@ -23,17 +25,17 @@ def binary_search(an_array, item)
   last = an_array.length - 1
 
   while first <= last
-      i = (first + last) / 2
+    i = (first + last) / 2
 
-      if an_array[i] == item
-          return "#{item} found at position #{i}"
-      elsif an_array[i] > item
-          last = i - 1
-      elsif an_array[i] < item
-          first = i + 1
-      else
-          return "#{item} not found in this array"
-      end
+    if an_array[i] == item
+      return "#{item} found at position #{i}"
+    elsif an_array[i] > item
+      last = i - 1
+    elsif an_array[i] < item
+      first = i + 1
+    else
+      return "#{item} not found in this array"
+    end
   end
 end
 
@@ -42,18 +44,18 @@ def binary_search_recursive(an_array, item)
   first = 0
   last = an_array.length - 1
 
-  if an_array.length == 0
-      return "#{item} was not found in the array"
+  if an_array.empty?
+    return "#{item} was not found in the array"
   else
-      i = (first + last) / 2
-      if item == an_array[i]
-          return "#{item} found"
+    i = (first + last) / 2
+    if item == an_array[i]
+      return "#{item} found"
+    else
+      if an_array[i] < item
+        return binary_search_recursive(an_array[i + 1, last], item)
       else
-          if an_array[i] < item
-              return binary_search_recursive(an_array[i+1, last], item)
-          else
-              return binary_search_recursive(an_array[first, i-1], item)
-          end
+        return binary_search_recursive(an_array[first, i - 1], item)
       end
+    end
   end
 end
