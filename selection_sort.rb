55ddf7713bc000e2d1df88bcddf42024842c2b6f
Walker-TW <thomas.w.walker@live.co.uk>
2020-04-07 16:50:19 +0100

    Added notes on selection_sort

--- selection_sort.rb
+++ selection_sort.rb
@@ -3,9 +3,13 @@ def selection_sort(array)
   (0...l).each do |i|
     min = i
     ((i + 1)...l).each do |j|
+      # do this to the next item unless the array index is at the minimum
+      # in which case cancel and return the entire array.
       next unless array[j] < array[min]
 
       temp = array[j]
+      # if the below line is not here then it will simply override the previous pointer
+      # this is because you need a tempory memory address to place it to refind it check CS50 notes
       array[j] = array[min]
       array[min] = temp
     end
